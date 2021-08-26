e822d547f5630b3d8235e2a8420786e702d6659b
Indigo Blood <iblood2@huskers.unl.edu>
2021-08-26 13:07:24 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Indigo Blood & Jace Pethoud
+ *         iblood2@huskers.unl.edu
+ * Date: 2021/8/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Indigo Blood & Jace Pethoud!\n");
+
+  return 0;
+}
